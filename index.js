dfef5a069443c6e15f212de2636a4d9f9af9dd86
Om-Govindani <omgovindani@gmail.com>
2026-03-07 04:15:53 +0530

    Added Outh initialization example in index.js

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ module.exports = {
     init , 
     register
 }
+
+
+/* 
+this is the example for initializing of the options
+outh.init({
+  model: User,
+  identifier: "email",
+  passwordField: "password",
+  jwtSecret: process.env.JWT_SECRET,
+
+  allowedFields: [
+    "email",
+    "password",
+    "name",
+    "phone",
+    "address",
+    "age",
+    "gender",
+    "pincode"
+  ]
+}); */
